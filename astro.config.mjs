879579fdcab115346bdca7824bf3f2d7e42c1b6d
софа <sofiafedotova2007@gmail.com>
2025-10-03 22:24:46 +0400

    add 2 pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,37 @@ import { defineConfig } from 'astro/config';
  import icon from "astro-icon";
 
 // https://astro.build/config
+// export default defineConfig({
+//   integrations: [icon()],
+// });
+
+
 export default defineConfig({
   integrations: [icon()],
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          // Configure asset file names
+          assetFileNames: (assetInfo) => {
+            let extType = assetInfo.names[0].split(".").at(1);
+
+            console.log(extType)
+
+            if (extType !== undefined) {
+              if (/png|jpe?g|svg|gif|webp|tiff|bmp|ico/i.test(extType)) {
+                extType = "img";
+              } else if (/woff|woff2|eot|ttf|otf/i.test(extType)) {
+                extType = "fonts";
+              } else if (/css/i.test(extType)) {
+                extType = "css";
+              }
+            }
+
+            return `assets/${extType}/[name][extname]`;
+          }
+        }
+      }
+    }
+  }
 });
